f5b19c2d149df2120089afaf7ff312647db2f0e6
kyhor <kyhor@ucsd.edu>
2016-04-19 07:39:41 +0800

    Sum

--- app/src/main/java/com/example/khor_000/testproject/MainActivity.java
+++ app/src/main/java/com/example/khor_000/testproject/MainActivity.java
@@ -1,22 +1,81 @@
 package com.example.khor_000.testproject;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText num1;
+    EditText num2;
+    TextView res;
+    int in1;
+    int in2;
+    int sum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        LayoutInflater inflater = (LayoutInflater) MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View layout = inflater.inflate(R.layout.content_main , (ViewGroup)findViewById(R.id.action_bar_root));
+
+
+        num1 = (EditText) layout.findViewById(R.id.tv1);
+        num1.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            public boolean onEditorAction(TextView tv, int i, KeyEvent keyEvent) {
+                boolean handled = false;
+                if (i == EditorInfo.IME_ACTION_NEXT) {
+
+                    return true;
+                }
+                return handled;
+            }
+        });
+
+        num2 = (EditText) layout.findViewById(R.id.tv2);
+        num2.setOnEditorActionListener(new TextView.OnEditorActionListener(){
+            public boolean onEditorAction(TextView tv, int i, KeyEvent keyEvent){
+                boolean handled = false;
+                if ( i == EditorInfo.IME_ACTION_NEXT){
+
+                    return true;
+                }
+                return handled;
+            }
+        });
+
+        final Button button = (Button) findViewById(R.id.sum);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                res = (TextView) layout.findViewById(R.id.result);
+
+                String input2 = num2.getText().toString();
+                in2 = Integer.parseInt(input2);
+
+                String input1 = num1.getText().toString();
+                in1 = Integer.parseInt(input1);
+
+                sum = sum(in1,in2);
+
+                res.setText("Sum: " + sum);
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +85,10 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public int sum(int a,int b){
+        return a+b;
     }
 
     @Override
