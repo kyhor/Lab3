be5868782fc9cfbceb6b438ad734a783b3234ed5
kyhor <kyhor@ucsd.edu>
2016-04-19 07:54:33 +0800

    sub

--- app/src/main/java/com/example/khor_000/testproject/MainActivity.java
+++ app/src/main/java/com/example/khor_000/testproject/MainActivity.java
@@ -91,6 +91,10 @@ public class MainActivity extends AppCompatActivity {
         return a+b;
     }
 
+    public  int sub(int a, int b){
+
+        return a-b;
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
